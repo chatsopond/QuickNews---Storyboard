a35776b8de8dbe9c9fb1e3c76e904a409d215d67
Chatsopon Deepateep <chatsopon_acc@icloud.com>
2023-03-16 16:54:50 +0700

    Removed unneeded parentheses and cleaned up unused code.

--- QuickNews-Storyboard/Extension/URLRequest+Extension.swift
+++ QuickNews-Storyboard/Extension/URLRequest+Extension.swift
@@ -14,25 +14,6 @@ struct Resource<T: Decodable> {
 }
 
 extension URLRequest {
-//  static func load<T>(resource: Resource<T>) -> Observable<T?> {
-//    let observable = Observable
-//      .from([resource.url])
-//      .flatMap { url -> Observable<Data> in
-//        let request = URLRequest(url: url)
-//        return URLSession.shared.rx.data(request: request)
-//      }
-//      .map { data -> T? in
-//        var result: T?
-//        do {
-//          result = try JSONDecoder().decode(T.self, from: data)
-//        } catch {
-//          print("\(#function) - error: \(error)")
-//        }
-//        return result
-//      }
-//      .asObservable()
-//    return observable
-//  }
   static func load<T: Decodable>(resource: Resource<T>) -> Observable<T> {
     let observable = Observable
       .just(resource.url)
@@ -40,7 +21,7 @@ extension URLRequest {
         let request = URLRequest(url: url)
         return URLSession.shared.rx.response(request: request)
       }
-      .map { (response, data) -> T in
+      .map { response, data -> T in
         if 200..<300 ~= response.statusCode {
           return try JSONDecoder().decode(T.self, from: data)
         } else {
