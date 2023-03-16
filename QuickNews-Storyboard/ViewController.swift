03d482800f7fdf9bd9b5a3117ae322f16bd4265b
Chatsopon Deepateep <chatsopon_acc@icloud.com>
2023-03-16 13:41:43 +0700

    Initial Commit

---
+++ QuickNews-Storyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuickNews-Storyboard
+//
+//  Created by Chatsopon Deepateep on 16/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
