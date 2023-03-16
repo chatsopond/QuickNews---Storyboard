2c1fd8056ee3ce553c83fd49a2ba891cf11b6e4c
Chatsopon Deepateep <chatsopon_acc@icloud.com>
2023-03-16 16:57:34 +0700

    Added `// swiftlint:disable:next force_unwrapping` to allow forced unwrapping of URL

--- QuickNews-Storyboard/Model/Article.swift
+++ QuickNews-Storyboard/Model/Article.swift
@@ -13,6 +13,7 @@ struct ArticleList: Decodable {
 
 extension ArticleList {
   static var all: Resource<ArticleList> = {
+    // swiftlint:disable:next force_unwrapping
     let url = URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=8bfba1b8763148feb3583622592d9115")!
     return Resource(url: url)
   }()
